b570706bf96fcc7beeff8e4fb70f37d8bec2b096
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-03 17:23:14 +1100

    react forms

---
+++ react-forms/src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function App() {
+
+const [name, setName] = useState("");
+const [headingText, setText] = useState("");
+
+function handleChange(event){ 
+  setName(event.target.value)
+}
+
+function handleClick(event){
+  setText(name)
+
+
+  event.preventDefault();
+}
+
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+
+      <form onSubmit={handleClick}>
+      <input 
+      onChange={handleChange} 
+      type="text" 
+      placeholder="What's your name?" 
+      value={name}
+      />
+      <button type="submit" onClick={handleClick}>Submit</button>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
