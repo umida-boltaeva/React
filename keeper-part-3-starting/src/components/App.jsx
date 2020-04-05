cdfde7a6f73c5a90b360fd50dccfe5a752aaac9b
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-05 19:14:26 +1000

    kepper app - implement the delete note functionality

--- keeper-part-3-starting/src/components/App.jsx
+++ keeper-part-3-starting/src/components/App.jsx
@@ -13,14 +13,24 @@ function addNote(note){
    return  [...prevNotes, note]; 
  });
 }
+function deleteNote(id){
+  setNotes(prevNotes => {
+    return  prevNotes.filter((noteItem, index) => {
+      return index !== id; 
+    })
+  })
+}
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote}/>
-      {notes.map(noteItem => {
-        return <Note 
+      {notes.map((noteItem, index) => {
+        return <Note
+          key={index}
+          id={index} 
           title={noteItem.title}
           content={noteItem.content}
+          onDelete={deleteNote}
         />
       })}
       <Footer />
--- keeper-part-3-starting/src/components/CreateArea.jsx
+++ keeper-part-3-starting/src/components/CreateArea.jsx
@@ -19,10 +19,12 @@ function CreateArea(props) {
 
   function submitNote(event){ 
     props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
     event.preventDefault();
   }
-
-
   return (
     <div>
       <form>
