013a8fde4c5b573007fcb281cdfb79b317878a87
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-05 18:07:02 +1000

    keeper app - implement the add note functionality

---
+++ keeper-part-3-starting/src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+
+  const [notes, setNotes] = useState([]);
+
+function addNote(note){
+ setNotes(prevNotes => {
+   return  [...prevNotes, note]; 
+ });
+}
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {notes.map(noteItem => {
+        return <Note 
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
